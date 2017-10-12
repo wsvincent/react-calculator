7a382e0af33f52a4bd59c38a78efcf503f7d1403
William Vincent <will@wsvincent.com>
2017-10-12 09:22:01 -0400

    import css, create index.css file

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import './ButtonPanel.css';
 
 class ButtonPanel extends Component {
   handleClick = buttonName => {
